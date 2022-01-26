235a42efb62102924fd35c0b09b9cd4874476778
Abdullah Ridwan <abdullahridwan73@gmail.com>
2022-01-25 19:37:30 -0500

    commit message from python script

--- test_script.py
+++ test_script.py
@@ -6,25 +6,9 @@ repo = git.Repo(os.getcwd())
 
 
 
-# List remotes
-print('Remotes:')
-for remote in repo.remotes:
-    print(f'- {remote.name} {remote.url}')
-
-print(repo.remotes.origin.name)
-
-
-
-
-
-# Run "git branch -r" and collect results into array
-remote_branches = []
-for ref in repo.git.branch('-r').split('\n'):
-    print(ref)
-    remote_branches.append(ref)
 
 repo.git.add('--all')  # to add all the working files.
 repo.git.commit('-m', 'commit message from python script', author='Abdullah Ridwan <abdullahridwan73@gmail.com>')
 origin = repo.remote(name='origin')
-print(origin.push())
+print(origin.push(refspec="master:master"))
 
