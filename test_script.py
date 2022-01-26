58fbd6c348cae9d372304f5a2c068066aada7df6
Abdullah Ridwan <abdullahridwan73@gmail.com>
2022-01-25 19:32:58 -0500

    commit message from python script

--- test_script.py
+++ test_script.py
@@ -26,5 +26,5 @@ for ref in repo.git.branch('-r').split('\n'):
 repo.git.add('--all')  # to add all the working files.
 repo.git.commit('-m', 'commit message from python script', author='Abdullah Ridwan <abdullahridwan73@gmail.com>')
 origin = repo.remote(name='origin')
-origin.push()
+print(origin.push())
 
