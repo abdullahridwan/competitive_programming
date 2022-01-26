575dee3502ce16dd53376d12d8eaa3f42cb65905
Abdullah Ridwan <abdullahridwan73@gmail.com>
2022-01-25 19:45:57 -0500

    commit message from python script

--- test_script.py
+++ test_script.py
@@ -1,14 +1,13 @@
 import os, git
 
-# Create repo for current directory
+
 repo = git.Repo(os.getcwd())
-
-
-
-
-repo.remotes.origin.pull()
-repo.git.add('--all')  # to add all the working files.
-repo.git.commit('-m', 'commit message from python script', author='Abdullah Ridwan <abdullahridwan73@gmail.com>')
-origin = repo.remote(name='origin')
-origin.push(refspec="main:main")
+if repo.is_dirty():
+    repo.remotes.origin.pull()
+    repo.git.add('--all')  # to add all the working files.
+    repo.git.commit('-m', 'commit message from python script', author='Abdullah Ridwan <abdullahridwan73@gmail.com>')
+    origin = repo.remote(name='origin')
+    origin.push(refspec="main:main")
+else:
+    print("Nothing to push in this folder")
 
