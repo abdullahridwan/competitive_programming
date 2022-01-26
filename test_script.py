7dc5cc5b5979d8ab42da37392e6650501433754c
Abdullah Ridwan <abdullahridwan73@gmail.com>
2022-01-25 19:32:26 -0500

    commit message from python script

--- test_script.py
+++ test_script.py
@@ -3,6 +3,20 @@ import os, git
 # Create repo for current directory
 repo = git.Repo(os.getcwd())
 
+
+
+
+# List remotes
+print('Remotes:')
+for remote in repo.remotes:
+    print(f'- {remote.name} {remote.url}')
+
+print(repo.remotes.origin.name)
+
+
+
+
+
 # Run "git branch -r" and collect results into array
 remote_branches = []
 for ref in repo.git.branch('-r').split('\n'):
